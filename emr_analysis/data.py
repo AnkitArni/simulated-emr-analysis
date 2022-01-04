ea1ef58c9f86d2108b29bac1b7798a4b52d54bd4
beml201 <beml201ex@gmail.com>
2022-01-04 12:45:25 +0000

    Added the load example function

--- emr_analysis/data.py
+++ emr_analysis/data.py
@@ -5,5 +5,18 @@ import typing as _ty
 import tkinter as _tk
 from zipfile import ZipFile as _zf
 
+_base = _os.path.dirname(_os.path.realpath(__file__))
+
 def load(option: int = 1) -> None:
     print('Hello there')
+
+def load_example():
+    dfs = dict()
+    with _zf(_base + "/examples/100-Patients.zip") as z:
+        files = [x for x in z.namelist() if x.endswith('.txt') and 'readme' not in x.lower()]
+        #print(files)
+        for file in files:
+            with z.open(file, 'r') as f:
+                key = file.replace('.txt', '')
+                dfs[key] = _pd.read_csv(f, header=0, delimiter='\t')
+    return dfs
