9e8afafc7cfee70b4cbf49e847853c9da3aac483
Arni, Ankit Manoj <aa1183@exeter.ac.uk>
2022-01-11 18:44:29 +0000

    Incremented version 0.1.0 -> 0.1.1

--- emr_analysis/__init__.py
+++ emr_analysis/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Ankit Arni, Brandon Lim, Luke Davies'
 __author_email__ = 'aa1183@exeter.ac.uk, beml201@exeter.ac.uk, led216@exeter.ac.uk'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['data', 'plot', 'summary']
 
 from . import *
